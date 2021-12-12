ac3281104b472db66bd23ad86d4c98ca225622e1
Jose Christopher Toscano <jctoscano1@gmail.com>
2021-12-12 00:54:05 -0800

    Changed tile color change to happen when user holds left click and hovers

--- Main.js
+++ Main.js
@@ -1,15 +1,42 @@
 const container = document.querySelector(".container");
+const resetBtn = document.querySelector(".reset-btn");
 
-for(let i = 0; i < 16*16; i++) {
-    const box = document.createElement("div");
-    box.classList.add('square');
-    container.appendChild(box);
+function generateGrid(rows) {
+    for(let i = 0; i < rows*rows; i++) {
+        const box = document.createElement("div");
+        box.classList.add('square');
+        container.appendChild(box);
+    }
 }
 
 function onHover(e) {
     const square = e.target.closest('.square');
     if(!square) return;
+    if(e.buttons !== 1) return;
     square.style.backgroundColor = 'black';
 }
 
+function onClick(e) {
+    const square = e.target.closest('.square');
+    if(!square) return;
+    square.style.backgroundColor = 'black';
+}
+
+function onReset(e) {
+    container.innerHTML = ``;
+    const input = Number(prompt("Enter new size"));
+    if(!input) {
+        alert("Must be a number");
+    } else if(input < 1 || input > 100) {
+        alert("Number must be between 1 and 100!");
+    } else {
+        container.style.gridTemplateColumns = `repeat(${input},auto`;
+        generateGrid(input);
+    }
+}
+
+generateGrid(16);
+
 container.addEventListener("mouseover", onHover);
+container.addEventListener("mousedown",onClick);
+resetBtn.addEventListener("click", onReset);
