54d7b72eb249032db8d339f2f61d45eb5ea11f4d
Jose Christopher Toscano <jctoscano1@gmail.com>
2021-12-12 14:30:03 -0800

    Added menu

--- Main.js
+++ Main.js
@@ -1,11 +1,8 @@
-const container = document.querySelector(".container");
-const resetBtn = document.querySelector(".reset-btn");
-
 function generateGrid(rows) {
     for(let i = 0; i < rows*rows; i++) {
         const box = document.createElement("div");
         box.classList.add('square');
-        container.appendChild(box);
+        gridContainer.appendChild(box);
     }
 }
 
@@ -13,31 +10,37 @@ function onHover(e) {
     const square = e.target.closest('.square');
     if(!square) return;
     if(e.buttons !== 1) return;
-    square.style.backgroundColor = 'black';
+    square.style.backgroundColor = color;
 }
 
-function onClick(e) {
+function onClick(e,color) {
     e.preventDefault();
     const square = e.target.closest('.square');
     if(!square) return;
-    square.style.backgroundColor = 'black';
+    square.style.backgroundColor = color;
 }
 
 function onReset(e) {
-    container.innerHTML = ``;
+    gridContainer.innerHTML = ``;
     const input = Number(prompt("Enter new size"));
     if(!input) {
         alert("Must be a number");
     } else if(input < 1 || input > 100) {
         alert("Number must be between 1 and 100!");
     } else {
-        container.style.gridTemplateColumns = `repeat(${input},auto`;
+        gridContainer.style.gridTemplateColumns = `repeat(${input},auto`;
         generateGrid(input);
     }
 }
 
+const gridContainer = document.querySelector(".grid-container");
+const resetBtn = document.querySelector(".reset-btn");
+let color = "#000";
+
 generateGrid(16);
 
-container.addEventListener("mouseover", onHover);
-container.addEventListener("mousedown", onClick);
+gridContainer.addEventListener("mouseover", onHover);
+gridContainer.addEventListener("mousedown", onClick);
 resetBtn.addEventListener("click", onReset);
+
+color = "#ed11a9";
