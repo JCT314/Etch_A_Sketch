cc5ca6ece6e4a5a28f42788be50c7993b931f301
Jose Christopher Toscano <jctoscano1@gmail.com>
2021-12-12 16:13:40 -0800

    User can change pen color

--- Main.js
+++ Main.js
@@ -1,4 +1,7 @@
 function generateGrid(rows) {
+    gridContainer.innerHTML = ``;
+    gridContainer.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+    gridContainer.grid
     for(let i = 0; i < rows*rows; i++) {
         const box = document.createElement("div");
         box.classList.add('square');
@@ -13,34 +16,37 @@ function onHover(e) {
     square.style.backgroundColor = color;
 }
 
-function onClick(e,color) {
+function onClick(e) {
     e.preventDefault();
     const square = e.target.closest('.square');
     if(!square) return;
     square.style.backgroundColor = color;
 }
 
-function onReset(e) {
-    gridContainer.innerHTML = ``;
-    const input = Number(prompt("Enter new size"));
-    if(!input) {
-        alert("Must be a number");
-    } else if(input < 1 || input > 100) {
-        alert("Number must be between 1 and 100!");
-    } else {
-        gridContainer.style.gridTemplateColumns = `repeat(${input},auto`;
-        generateGrid(input);
-    }
-}
-
 const gridContainer = document.querySelector(".grid-container");
-const resetBtn = document.querySelector(".reset-btn");
+const currentColorDisplay = document.querySelector(".current-color-display");
+currentColorDisplay.value = '#000';
+const buttons = document.querySelectorAll('.menu-btn');
+const colorButton = buttons[0];
+const rainbowButton = buttons[1];
+const eraseButton = buttons[2];
+const clearButton = buttons[3];
+const slider = document.querySelector('.slider');
+const sliderDisplay = document.querySelector('.slider-display');
 let color = "#000";
 
 generateGrid(16);
 
 gridContainer.addEventListener("mouseover", onHover);
 gridContainer.addEventListener("mousedown", onClick);
-resetBtn.addEventListener("click", onReset);
-
-color = "#ed11a9";
+currentColorDisplay.addEventListener("change",(e) => {
+    color = e.currentTarget.value;
+});
+slider.addEventListener("input", (e) => {
+    const value = e.currentTarget.value;
+    sliderDisplay.textContent = `${value} x ${value}`;
+});
+slider.addEventListener("change", (e) => {
+    const value = e.currentTarget.value;
+    generateGrid(value);
+});
